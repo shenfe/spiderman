de41f1eb2418a6a80d4285afda34eeadb99ba3d2
hengwu <youchuangqiba@163.com>
2018-02-06 22:49:38 +0800

    upd8

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,25 +4,27 @@ mongoose.connect('mongodb://localhost/test');
 
 const db = mongoose.connection;
 
+const { user } = require('./schema');
+
 db.on('error', console.error.bind(console, 'connection error: '));
 
 db.once('open', function () {
     console.log('mongoose opened!');
-    var userSchema = new mongoose.Schema({
-        name: { type: String, unique: true },
-        password: String
-    }, { collection: 'test' }
-    );
-    var User = mongoose.model('test', userSchema);
+
+    const userSchema = new mongoose.Schema(user, { collection: 'test' });
+
+    const User = mongoose.model('test', userSchema);
 
     User.findOne({ name: 'WangEr' }, function (err, doc) {
-        if (err) console.log(err);
-        else console.log(doc.name + ', password - ' + doc.password);
+        err
+        ? console.log(err)
+        : console.log(doc.name + ', ' + doc.password);
     });
 
-    var lisi = new User({ name: 'LiSi', password: '123456' });
+    const lisi = new User({ name: 'ZhangSan', password: '123456' });
     lisi.save(function (err, doc) {
-        if (err) console.log(err);
-        else console.log(doc.name + ' saved');
+        err
+        ? console.log(err)
+        : console.log(doc.name + ' saved');
     });
 });
---
+++ src/database/schema.js
@@ -0,0 +1,8 @@
+const user = {
+    name: { type: String, unique: true },
+    password: String
+};
+
+module.exports = {
+    user
+};
